850e0894f45444652d1f998dd175b76af0bf37de
Seubmarine <seubmarinepro@gmail.com>
2022-01-15 01:59:36 +0100

    printf return number of char to output

--- printf.c
+++ printf.c
@@ -6,63 +6,81 @@
 /*   By: tbousque <tbousque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 20:03:33 by tbousque          #+#    #+#             */
-/*   Updated: 2021/11/24 20:35:02 by tbousque         ###   ########.fr       */
+/*   Updated: 2022/01/15 01:42:07 by tbousque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "printf.h"
-#include <string.h>
-#include <stdio.h>
-#include <unistd.h>
+
+int get_int_size_in_base(int n, int base)
+{
+	int	size;
+
+	size = 0 + (1 * n < 0);
+	while (n)
+	{
+		n /= base;
+		size++;
+	}
+	
+	return (size);
+}
+
+int get_pointer_size(void *ptr)
+{
+	int					size;
+	unsigned long long	n;
+
+	if (ptr == NULL)
+		return (sizeof(NULL_PTR_STR) - 1);
+	n = (unsigned long long) ptr;
+	size = 2;
+	while (n)
+	{
+		n /= 16;
+		size++;
+	}
+	return (size);
+}
+
+int	get_size_of_entire_string(const char *str, va_list arg)
+{
+	int	size;
+
+	size = 0;
+	while(*str)
+	{
+		if (*str == '%')
+		{	
+			str++;
+			if (*str == 's')
+				size += ft_strlen(va_arg(arg, char *));
+			else if (*str == 'c' || *str == '%')
+				size++;
+			else if (*str == 'x' || *str == 'X')
+				size += get_int_size_in_base(va_arg(arg, int), 16);
+			else if (*str == 'd' || *str == 'i')
+				size += get_int_size_in_base(va_arg(arg, int), 10);
+			else if (*str == 'u')
+				size += get_int_size_in_base((unsigned int) va_arg(arg, unsigned int), 10);
+			else if (*str == 'p')
+				size += get_pointer_size(va_arg(arg, void *));
+		}
+		else
+			size++;
+		str++;
+	}
+	return (size);
+}
 
 int	ft_printf(const char *str, ...)
 {
 	va_list aq;
 	va_list ap;
-	void *current_arg;
+	int		size;
 
 	va_start(ap, str);	
 	va_copy(aq, ap);
-	while (*str)
-	{
-		if (*str == '%')
-		{
-			str++;
-			while (*str == ' ')
-				str++;
-			if (*str == 'd')
-				ft_putnbr_fd(va_arg(aq, int), 1);
-			else if (*str == 'c')
-				ft_putchar_fd(va_arg(aq, int), 1);
-			else if (*str == 's')
-				ft_putstr_fd(va_arg(aq, char *), 1);
-			else if (*str == '%')
-				ft_putchar_fd('%', 1);
-			else if (*str == 'p' || *str == 'x')
-			{
-				char *hex = ft_ullbase(va_arg(aq, unsigned long long), "0123456789abcedf", 16, 0);
-				if (*str == 'p')
-					write(1, "0x", 2);
-				ft_putstr_fd(hex, 1);
-				free(hex);
-			}
-			str++;
-		}
-		if (!*str)
-			break;
-		write(1, str, 1);
-		str++;
-	}
+	size = get_size_of_entire_string(str, aq);
 	va_end(aq);
-	return (0);
-}
-
-int	main(int argc, char **argv)
-{
-	int n = 42;
-	const char *str = "Bonjour je m'appel %s j'ai %d ans\npointer of %d is %p\n";
-	printf(str, "Théo", 19, n, &n);
-	ft_printf(str, "Théo", 19, n, &n);
-	printf("linux return : %p\n", NULL);
-	ft_printf("I return : %p\n", NULL);
-	return (0);
+	return (size);
 }
--- printf.h
+++ printf.h
@@ -6,7 +6,7 @@
 /*   By: tbousque <tbousque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 13:49:40 by tbousque          #+#    #+#             */
-/*   Updated: 2021/11/24 13:51:02 by tbousque         ###   ########.fr       */
+/*   Updated: 2022/01/15 01:57:56 by tbousque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,12 @@
 # define PRINTF_H
 # include <stdarg.h>
 # include "libft.h"
+
+# if __linux__
+#  define NULL_PTR_STR "(nil)"
+# else
+#  define NULL_PTR_STR "0x0"
+# endif
+
+int	ft_printf(const char *, ...);
 #endif
